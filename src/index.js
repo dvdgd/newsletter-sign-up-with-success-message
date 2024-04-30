7649a74742e5f43c31d9d213c415ce24617f6ba1
dvdgd <dvdgd@outlook.com.br>
2024-04-30 19:27:11 -0300

    mobile first

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const validateEmail = () => {
+  const form = document.getElementById('email-subscription-form');
+  const emailInput = document.getElementById('email');
+  const email = emailInput.value;
+  
+  const validation = emailInput.checkValidity();
+  if (validation) {
+    form.classList.remove('error-state');
+    return email;
+  }
+  
+  let message = '';
+  if (email === '' || !email) {
+    message = 'Email is required';
+  } else {
+    message = 'Valid email required';
+  }
+
+  const errorElement = form.querySelector('span.error-message');
+  errorElement.textContent = message;
+  form.classList.add('error-state')
+
+  return false;
+}
+
+const handleEmailSubmit = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const email = validateEmail();
+  if (!email) {
+    return;
+  }
+
+  const articleSucess = document.querySelector('article.success');
+  const strongElement = articleSucess.querySelector('strong');
+  strongElement.textContent = email;
+
+  const currentArticle = document.querySelector('article.active');
+
+  currentArticle.classList.toggle('active');
+  articleSucess.classList.toggle('active');
+}
+
+
+const onDismissMessage = (e) => {
+  const article = e.target.closest('article');
+  article.classList.remove('active');
+
+  document.querySelector('article:not(.success)').classList.add('active');
+}
+
+//validate the email on fileld change 
+document.getElementById('email').addEventListener('input', () => {
+  validateEmail();
+});
+
+document.getElementById('email-subscription-form').addEventListener('submit', handleEmailSubmit);
+
+document.querySelector('article.success button').addEventListener('click', onDismissMessage);
